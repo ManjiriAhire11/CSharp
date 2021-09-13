4a083fc1786164c66af9e5a4f68b9edc8aa9a39b
ManjiriAhire11 <90183145+ManjiriAhire11@users.noreply.github.com>
2021-09-14 02:21:28 +0530

    Add files via upload

---
+++ Day2ASS-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2.Accept no from user and write table as follows. For example user types 19 then output should be:
+
+
+namespace Day2ASS_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num + "*" + i+"="+ num * i);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}

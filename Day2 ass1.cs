cd4dc301c87625a42e72f85a024ff85ef9541d7b
ManjiriAhire11 <90183145+ManjiriAhire11@users.noreply.github.com>
2021-09-14 02:20:51 +0530

    Add files via upload

---
+++ Day2 ass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    //What will be the output of below code:
+
+namespace Day2Ass_1
+{
+  public  class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(12.5));
+            Console.WriteLine(Math.Round(10.5));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+//Output:
+//12
+//10

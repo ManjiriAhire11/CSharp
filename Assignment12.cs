869f68d689a391a8b64a0ee2c8e6d75d4a8ed583
ManjiriAhire11 <90183145+ManjiriAhire11@users.noreply.github.com>
2021-09-14 02:29:39 +0530

    Add files via upload

---
+++ Assignment12.cs
@@ -0,0 +1,46 @@
+﻿//Rectify compile time errors if any , let us know the output of following code:
+
+using System;
+class samplee
+{
+    public static void IFutureMethod()
+    {
+        Console.WriteLine("IFutureMethod method");
+    }
+    public void ZensarMethod(string s)
+    {
+        first();
+        Console.WriteLine("ZensarMethod method");
+    }
+
+    public void first()
+    {
+
+    }
+
+    public void ZensarMethod(int i)
+    {
+        Console.WriteLine(i);
+        ZensarMethod();
+    }
+
+    private void ZensarMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        samplee obj = new samplee();
+        obj.first();
+        obj.ZensarMethod("Akkki");
+        Console.Read();
+    }
+}
+
+
+ 
+           
+                

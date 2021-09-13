e194c830aea7baca8423225d185d4b76732b606e
ManjiriAhire11 <90183145+ManjiriAhire11@users.noreply.github.com>
2021-09-14 02:25:29 +0530

    Add files via upload

---
+++ Assignment7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    class Program
+    {
+     public   static void Main(string[] args)
+        {
+
+
+           
+                NamesDisplay("Manjiri","Ashu");
+                NamesDisplay("Neha", "Paritosh", "Riya", "Madhavi", "Kunal");
+                NamesDisplay("Kritika","Samidha","Dipesh","Pradnesh","Ritesh");
+
+                Console.ReadLine();
+
+            }
+
+            private static void NamesDisplay(params string[] parameters)
+            {
+                Console.WriteLine("****Students names are as follow****");
+
+                foreach (var item in parameters)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+        }
+
+    }
+
+
+
+           
